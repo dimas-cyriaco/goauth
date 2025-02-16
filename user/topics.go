1c475032164c8f214dae81773a3f610cf9723dd9
Dimas Cyriaco <dimascyriaco@icloud.com>
2025-02-16 11:15:50 -0300

    refactor(user): remove commented code

--- user/topics.go
+++ user/topics.go
@@ -2,12 +2,6 @@ package user
 
 import "encore.dev/pubsub"
 
-// type SignupEvent struct{ UserID int }
-//
-// var Signups = pubsub.NewTopic[*SignupEvent]("signups", pubsub.TopicConfig{
-// 	DeliveryGuarantee: pubsub.AtLeastOnce,
-// })
-
 type EmailVerificationRequestedEvent struct{ UserID int }
 
 var EmailVerificationRequested = pubsub.NewTopic[*EmailVerificationRequestedEvent]("email-verification", pubsub.TopicConfig{
